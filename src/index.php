dc4855b8c87e856422904bea9896e3f688dc3f43
Maria Dadalt <maria@tri.be>
2021-09-14 07:13:59 -0300

    initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello there, my friend</title>
+    <style>
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+    </style>
+</head>
+<body>
+<img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+<?php echo "I'll list all projects here" ?>
+</body>
+</html>
